808cfc6c855af7febf18432ca9c10a1db953c0dc
yerramsneha <80068238+yerramsneha@users.noreply.github.com>
2022-05-20 13:16:35 +0530

    added datatypes.java file

---
+++ datatypes.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class datatypes 
+{
+	public static void main(String args[])
+	{
+		int num = 5; // integer
+		float f = 5.99f; // floating
+		char c = 'J'; // character
+		boolean bool = true;
+		String s = "Java";
+		System.out.println(num);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(bool);
+		System.out.println(s);
+		int n = 9;
+		double d = n; // widenning casting
+		System.out.println(n);
+		System.out.println(d);
+		double d1= 9.78d;
+		int n1 = (int) d1; // narrow casting
+		System.out.println(d1);
+		System.out.println(n1);
+	}
+}
